67338d2e82793bf8a985ac531921920f8f3fe4dc
LSH0809 <skyey94@naver.com>
2021-05-31 13:43:50 +0900

    search condition update

--- project/src/main/java/com/page27/project/repository/ItemRepositoryImpl.java
+++ project/src/main/java/com/page27/project/repository/ItemRepositoryImpl.java
@@ -57,27 +57,53 @@ public class ItemRepositoryImpl implements ItemRepositoryCustom {
 
     @Override
     public Page<ItemDto> searchAllItemByCondition(SearchItem search, Pageable pageable) {
-        QueryResults results = queryFactory
-                .select(new QItemDto(
-                        QItem.item.id,
-                        QItem.item.itemName,
-                        QItem.item.firstCategory,
-                        QItem.item.price,
-                        QItem.item.saleStatus,
-                        QItem.item.imgUrl,
-                        QItem.item.color,
-                        QItem.item.rep
-                ))
-                .from(QItem.item)
-                .where(QItem.item.rep.eq(true),saleStatusEq(search.getSalestatus()),cmodeEq(search.getCmode()),itemNameEq(search.getItem_name()))
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetchResults();
+        System.out.println("here 1 + " + search.getCmode());
+        if(search.getCmode().equals("whole")){
+            QueryResults results = queryFactory
+                    .select(new QItemDto(
+                            QItem.item.id,
+                            QItem.item.itemName,
+                            QItem.item.firstCategory,
+                            QItem.item.price,
+                            QItem.item.saleStatus,
+                            QItem.item.imgUrl,
+                            QItem.item.color,
+                            QItem.item.rep
+                    ))
+                    .from(QItem.item)
+                    .where(QItem.item.rep.eq(true),saleStatusEq(search.getSalestatus()),itemNameEq(search.getItem_name()))
+                    .offset(pageable.getOffset())
+                    .limit(pageable.getPageSize())
+                    .fetchResults();
+            System.out.println("here 2 + " + search.getCmode());
+            List<ItemDto> content = results.getResults();
+            long total = results.getTotal();
 
-        List<ItemDto> content = results.getResults();
-        long total = results.getTotal();
+            return new PageImpl<>(content, pageable, total);
+        }
+        else {
+            QueryResults results = queryFactory
+                    .select(new QItemDto(
+                            QItem.item.id,
+                            QItem.item.itemName,
+                            QItem.item.firstCategory,
+                            QItem.item.price,
+                            QItem.item.saleStatus,
+                            QItem.item.imgUrl,
+                            QItem.item.color,
+                            QItem.item.rep
+                    ))
+                    .from(QItem.item)
+                    .where(QItem.item.rep.eq(true), saleStatusEq(search.getSalestatus()),cmodeEq(search.getCmode()), itemNameEq(search.getItem_name()))
+                    .offset(pageable.getOffset())
+                    .limit(pageable.getPageSize())
+                    .fetchResults();
 
-        return new PageImpl<>(content, pageable, total);
+            List<ItemDto> content = results.getResults();
+            long total = results.getTotal();
+
+            return new PageImpl<>(content, pageable, total);
+        }
     }
 
     private BooleanExpression saleStatusEq(String saleStatusCondition){
