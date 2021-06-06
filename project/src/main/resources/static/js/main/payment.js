8f2e2993040ae511c4075121f9c0db5d1c493943
chaechangwan <chang940406@naver.com>
2021-06-06 20:48:32 +0900

    admin_order 수정 등

---
+++ project/src/main/resources/static/js/main/payment.js
@@ -0,0 +1,90 @@
+$(function(){
+    let total_price = 0
+    let discount = 0
+    const canuse = parseInt($('.canuse').val());
+    const mileage_input = 'input[name=used_mileage]';
+
+    // 초기 총 주문금액 및 결제 예정금액 산출
+    $('.item_price').each(function(){
+        let order_price = parseInt($(this).val()) * parseInt($(this).siblings('.item_quantity').val());
+        $(this).closest('tr').find('.order_price').html(order_price.toLocaleString()); //한 아이템에 대한 총 가격 세팅
+        $(this).closest('tr').find('.order_mileage').html((order_price/100).toLocaleString());
+        total_price += order_price;
+    })
+    $('input[name=total_price]').val(total_price);
+    $('#total').html(total_price.toLocaleString());
+    $('input[name=tobepaid_price]').val(total_price)
+    $('#tobepaid').html(total_price.toLocaleString());
+
+    //체크박스 전체선택 or 해제
+    $('.check_all').click(function(){
+        if($('.check_all').is(":checked")){
+            $('.check').prop('checked', true)
+        }else {
+            $('.check').prop('checked', false)
+        }
+    })
+    
+    //체크박스 선택 삭제
+    $('.deletebtn').click(function(){
+        $('.check').each(function(){
+            if($(this).is(':checked')){
+                let tr = $(this).closest('tr');
+                total_price -= parseInt(tr.find('.item_price').val()) * parseInt(tr.find('.item_quantity').val()); 
+                tr.remove();
+                $('input[name=total_price]').val(total_price);
+                $('#total').html(total_price);
+                $('input[name=tobepaid_price]').val(total_price-discount);
+                $('#tobepaid').html(total_price-discount);
+            }
+        })
+        if($('.check').length == 0){    //상품이 전부 삭제되었을땐 메인화면으로 튕겨낸다.
+            location.href = './index.html';
+        }
+    })
+    
+    // 마일리지 적용
+    $(mileage_input).keyup(function(){
+        discount = parseInt($(this).val() ? $(this).val() : 0);
+        if(discount > canuse){
+            discount = canuse;
+        }
+        if(discount <= total_price){    
+            $('#discounted').html(discount.toLocaleString());
+            $('input[name=used_mileage]').val(discount);
+            $('#tobepaid').html((total_price-discount).toLocaleString());
+            $('input[name=tobepaid_price]').val(total_price-discount);
+        }
+    })
+
+    // 배송정보 선택 AJAX
+    $('input[name=address_name]').click(function(){
+        console.log($(this).val());
+        if($(this).val() == 'new_addr'){
+            $('#address_recipient').val('');
+            $('#sample6_postcode').val('');
+            $('#sample6_address').val('');
+            $('#sample6_detailAddress').val('');
+            $('.address_home_number:not(:first-child)').val('');
+            $('.address_phone_number:not(:first-child)').val('');
+        }else{
+
+            let addr = {addr_name : $(this).val()}
+
+            $.ajax({
+                url:"/dataSend",
+                type:'POST',
+                data: JSON.stringify(addr),
+                contentType: 'application/json',
+                success:function(data){
+                    alert(data);
+                    //서버로부터의 응답을 이용해 배송 정보 렌더링.
+                },
+                error:function(e){
+                    alert(e.responseText);
+                }
+            })        
+        }
+    })
+    
+})
---
+++ project/src/main/resources/static/js/main/sendit.js
@@ -0,0 +1,62 @@
+function sendit(){
+    //alert("sendit호출");
+    if($('#address_name').val() == ""){
+        alert("배송지명 항목은 필수 입력값입니다.");
+        $('#address_name').focus();
+        return false;
+    }
+    if($('#address_recipient').val() == ""){
+        alert("성명 항목은 필수 입력값입니다.");
+        $('#address_recipient').focus();
+        return false;
+    }
+    if($('#sample6_postcode').val() == ""){
+        alert("주소 항목은 필수 입력값입니다.");
+        $('#sample6_postcode').focus();
+        return false;
+    }
+
+    let isempty = false;
+    $('.address_home_number').each(function(){
+        if($(this).val() == ""){
+            alert("일반전화 항목은 필수 입력값입니다.");
+            $(this).focus();
+            isempty = true
+            return false;       //foreach문 탈출
+        }
+    })
+    if(isempty){
+        return false;
+    }
+    $('.address_phone_number').each(function(){
+        if($(this).val() == ""){
+            alert("휴대전화 항목은 필수 입력값입니다.");
+            $(this).focus();
+            isempty = true
+            return false;
+        }
+    })
+    if(isempty){
+        return false;
+    }
+
+    //상품 번호 및 수량 데이터  JSON으로 만들어주기
+
+    let orderiteminfo = [];
+    let item_keys = $('.item_idx').get();
+    let item_colors = $('.item_color').get();
+    let item_quantities = $('.item_quantity').get();
+    console.log
+    for(let i=0; i < item_keys.length; i++){
+        let item_obj = {}
+        let key = $(item_keys[i]).val();
+        let color = $(item_colors[i]).val();
+        let quan = $(item_quantities[i]).val();
+        item_obj = {item_idx:key, item_color: color, item_quantity:quan}
+        orderiteminfo.push(item_obj);
+    }
+    alert(JSON.stringify(orderiteminfo));
+    $('input[name=orderiteminfo]').val(JSON.stringify(orderiteminfo));
+
+
+}
